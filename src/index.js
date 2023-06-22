b7e342f9b30ab1f697c413f7707beeaa294841ed
hari <harikarumuri1086@gmail.com>
2023-06-22 17:20:36 +0530

    updated branch and removed admin

--- src/index.js
+++ src/index.js
@@ -1,26 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { Provider } from 'react-redux';
-import { configureStore } from '@reduxjs/toolkit';
-import authReducer from './store/slices/authSlice';
-import pgsReducer from './store/slices/pgsSlice';
-import pgReducer from './store/slices/pgSlice';
 
-const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    pgs: pgsReducer,
-    pg: pgReducer,
-  },
-});
 
 
 ReactDOM.render(
-  <Provider store={store}>
+ 
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </Provider>,
+  ,
   document.getElementById('root')
 );
