a0aede9653c858a63e107e06a3d8891541aac865
Hari <harikarumuri1086@gmail.com>
2023-08-28 00:34:56 +0530

    changes nav bar

--- src/components/navBar/NavBar.js
+++ src/components/navBar/NavBar.js
@@ -1,7 +1,6 @@
-import React , {useState ,useEffect} from "react";
-import Logo from "../../assets/images/popular pg logo.png"
+import React, { useState, useEffect } from "react";
+import Logo from "../../assets/images/popular pg logo.png";
 import { Link } from "react-router-dom";
-
 
 const NavBar = () => {
   const [navbarActive, setNavbarActive] = useState(false);
@@ -16,71 +15,80 @@ const NavBar = () => {
 
   const handleScroll = () => {
     if (window.scrollY >= 400) {
-      document.querySelector('.header-bottom').classList.add('active');
+      document.querySelector(".header-bottom").classList.add("active");
     } else {
-      document.querySelector('.header-bottom').classList.remove('active');
+      document.querySelector(".header-bottom").classList.remove("active");
     }
   };
 
-  window.addEventListener('scroll', handleScroll);
+  window.addEventListener("scroll", handleScroll);
 
-return (
-    
-    
-
-  <div className="header-bottom">
-  <div className="container">
-    <Link to="/" className="logo">
-      <img src={Logo} alt="Homeverse logo" height="55px"/>
-    </Link>
-
-    <nav className={`navbar ${navbarActive ? 'active' : ''}`}>
-      <div className="navbar-top">
+  return (
+    <div className="header-bottom">
+      <div className="container">
         <Link to="/" className="logo">
-          <img src={Logo} alt="Homeverse logo" height="40px"  />
+          <img src={Logo} alt="Homeverse logo" height="55px" />
         </Link>
-        <button className="nav-close-btn" onClick={closeNavbar} aria-label="Close Menu">
-          <ion-icon name="close-outline"></ion-icon>
-        </button>
-      </div>
-      <div className="navbar-bottom">
-        <ul className="navbar-list">
-          <li>
-            <Link to="/" className="navbar-link" onClick={closeNavbar}>
-              Home
+
+        <nav className={`navbar ${navbarActive ? "active" : ""}`}>
+          <div className="navbar-top">
+            <Link to="/" className="logo">
+              <img src={Logo} alt="Homeverse logo" height="40px" />
             </Link>
-          </li>
-          {/* <li>
+            <button
+              className="nav-close-btn"
+              onClick={closeNavbar}
+              aria-label="Close Menu"
+            >
+              <ion-icon name="close-outline"></ion-icon>
+            </button>
+          </div>
+          <div className="navbar-bottom">
+            <ul className="navbar-list">
+              <li>
+                <Link to="/" className="navbar-link" onClick={closeNavbar}>
+                  Home
+                </Link>
+              </li>
+              {/* <li>
             <Link to="/about" className="navbar-link" onClick={closeNavbar}>
               About
             </Link>
           </li> */}
-          {/* <li>
+              {/* <li>
             <Link to="/service" className="navbar-link" onClick={closeNavbar}>
               Service
             </Link>
           </li> */}
-          <li>
-            <Link to="/listings" className="navbar-link" onClick={closeNavbar}>
-              Explore Pg
-            </Link>
-          </li>
-          {/* <li>
+              <li>
+                <Link
+                  to="/listings"
+                  className="navbar-link"
+                  onClick={closeNavbar}
+                >
+                  Explore Pg
+                </Link>
+              </li>
+              {/* <li>
             <Link to="/blog" className="navbar-link" onClick={closeNavbar}>
               Blog
             </Link>
           </li> */}
-          <li>
-            <Link to="/contact" className="navbar-link" onClick={closeNavbar}>
-              Contact
-            </Link>
-          </li>
-        </ul>
-      </div>
-    </nav>
+              <li>
+                <Link
+                  to="/contact"
+                  className="navbar-link"
+                  onClick={closeNavbar}
+                >
+                  Contact
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
 
-    <div className="header-bottom-actions">
-      {/* <button className="header-bottom-actions-btn" aria-label="Search">
+        <div className="header-bottom-actions">
+          {/* <button className="header-bottom-actions-btn" aria-label="Search">
         <ion-icon name="search-outline"></ion-icon>
         <span>Search</span>
       </button>
@@ -88,21 +96,21 @@ return (
         <ion-icon name="person-outline"></ion-icon>
         <span>Profile</span>
       </button> */}
-      <div className="wrapper">
-          <button className="header-top-btn">Add Listing</button>
+          <div className="wrapper">
+            <button className="header-top-btn AddListing">Add Listing</button>
           </div>
-      
-      <button
-        className="header-bottom-actions-btn"
-        onClick={toggleNavbar}
-        aria-label="Open Menu"
-      >
-        <ion-icon name="menu-outline"></ion-icon>
-        <span>Menu</span>
-      </button>
+
+          <button
+            className="header-bottom-actions-btn"
+            onClick={toggleNavbar}
+            aria-label="Open Menu"
+          >
+            <ion-icon name="menu-outline"></ion-icon>
+            <span>Menu</span>
+          </button>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
   );
 };
 
