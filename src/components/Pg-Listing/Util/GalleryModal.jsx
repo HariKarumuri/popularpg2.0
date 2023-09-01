7c48ad3c9ec8e2e32b4f44580655a58902280e00
sushskvnitn <sushantkogurwar@gmail.com>
2023-09-01 20:12:39 +0530

    updated search and added modal

--- src/components/Pg-Listing/Util/GalleryModal.jsx
+++ src/components/Pg-Listing/Util/GalleryModal.jsx
@@ -1,19 +1,61 @@
 import React from "react";
 
-const GalleryModal = ({ images, heading, onClose }) => {
+const GalleryModal = ({ images, title, Coverimage }) => {
+  const sortBySize = (a, b) => {
+    const sizeA = a.width * a.height;
+    const sizeB = b.width * b.height;
+    return sizeB - sizeA; 
+  };
+  images.sort(sortBySize);
+
   return (
-    <div className="gallery-modal ">
-      <div className="gallery-modal-content">
-        <button className="close-btn" onClick={onClose}>
-          &times;
-        </button>
-        <h3>{heading}</h3>
-        <div className="gallery-images">
-          {images.map((image, index) => (
-            <div className="gallery-image" key={index}>
-              <img src={image.image} alt={`Image ${index}`} className="img-fluid gallery-img"  />
+    <div
+      className="modal fade"
+      id="staticBackdrop"
+      data-bs-backdrop="static"
+      data-bs-keyboard="false"
+      tabIndex="-1"
+      aria-labelledby="staticBackdropLabel"
+      aria-hidden="true"
+    >
+      <div className="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h4 className="headings text-center">{title}</h4>
+            <button
+              type="button"
+              className="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div className="modal-body">
+            <div className="row">
+              {images.length > 0 ? (
+                images.map((image, index) => (
+                  <div className="col-md-4" key={index}>
+                    <div className="gallery-image">
+                      <img
+                        src={image.image}
+                        alt={image.alt}
+                        className="gallery-image-img"
+                      />
+                    </div>
+                  </div>
+                ))
+              ) : (
+                <div className="col-md-4" key={1}>
+                  <div className="gallery-image">
+                    <img
+                      src={Coverimage}
+                      alt="Coverimages"
+                      className="gallery-image-img"
+                    />
+                  </div>
+                </div>
+              )}
             </div>
-          ))}
+          </div>
         </div>
       </div>
     </div>
