0964a4a083b9eccb3fbdc40622c3c7f73fffaeec
Hari <harikarumuri1086@gmail.com>
2023-10-23 12:29:51 +0530

    pagination added and side ads functionality done

--- src/components/Pg-Listing/Cards/AdsCard.js
+++ src/components/Pg-Listing/Cards/AdsCard.js
@@ -1,28 +1,64 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const AdsCard = () => {
+  const [ads, setAds] = useState([]);
+
+  useEffect(() => {
+    const fetchAds = async () => {
+      try {
+        const response = await axios.get("https://popularpg.in/side_ads/");
+        setAds(response.data);
+      } catch (error) {
+        console.error("Error fetching ads:", error);
+      }
+    };
+
+    fetchAds();
+  }, []);
+
   return (
     <div>
-      <div
-        className="card my-2"
-        style={{ width: "8rem", borderRadius: 0, marginLeft: "-1rem" }}
-      >
-        <img
-          className="img-fluid"
-          style={{ width: "8rem",height: "8rem",borderRadius: 0 }}
-          src="dummy"
-          alt="Card imag cap"
-        />
-        <div className="mx-2">
-          <div className=" text-secondary fw-bold" style={{ fontSize:"13px" }}>
-            Dozo Moon Coliving PG
+      {ads.map((ad, index) => (
+        <div
+          className="card my-2"
+          style={{ width: "8rem", borderRadius: 0, marginLeft: "-1rem" }}
+          key={index}
+        >
+          <img
+            className="img-fluid"
+            style={{ width: "8rem", height: "8rem", borderRadius: 0 }}
+            src={ad.product.cover_image}
+            alt="Card imag cap"
+          />
+          <div className="mx-2">
+            <div
+              className="text-secondary fw-bold"
+              style={{ fontSize: "13px" }}
+            >
+              {ad.product.product_name}
+            </div>
+            <div className="desc text-muted fw-bold" style={{ fontSize: "13px" }}>
+              For {ad.product.pg_for}
+            </div>
+            <div className="desc text-muted " style={{ fontSize: "13px" }}>
+              {ad.product.city}
+            </div>
+            <div className="desc " style={{ fontSize: "12px" }}>
+              {ad.product.single_sharing
+                ? "Single Sharing"
+                : ad.product.double_sharing
+                ? "Double Sharing"
+                : ad.product.triple_sharing
+                ? "Triple Sharing"
+                : "Multiple Sharing"}
+            </div>
+            <div className="text-dark fw-bold">
+              Rs. {ad.product.min_price}
+            </div>
           </div>
-          <div className="desc text-muted fw-bold" style={{ fontSize:"13px" }} >For boys & girls</div>
-          <div className="desc text-muted " style={{ fontSize:"13px" }} >Banglore</div>
-          <div className="desc " style={{ fontSize:"12px" }} >Single , Double & Triple Sharing</div>
-          <div className=" text-dark fw-bold" >Rs. 4000</div>
         </div>
-      </div>
+      ))}
     </div>
   );
 };
--- src/components/Pg-Listing/Pg_Product_List.js
+++ src/components/Pg-Listing/Pg_Product_List.js
@@ -1,19 +1,37 @@
-import React, {useContext } from "react";
+import React, { useContext, useState } from "react";
 import Pg_Listing_Card from "./Cards/Pg_Listing_Card";
-import AdsCard from './Cards/AdsCard';
+import AdsCard from "./Cards/AdsCard";
 import SearchBar from "../SearchBar/SearchBar";
 import SearchContext from "../Appcontext/SearchContext";
+
 const Pg_Product_List = () => {
-  const {
-    pgs,
-    isLoading, 
-  }=useContext(SearchContext);
+  const { pgs, isLoading } = useContext(SearchContext);
+  const itemsPerPage = 30; // Number of items per page
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
+    scrollToTop(); // Scroll to top when paginate is called
+  };
+
+  const currentItems = pgs.slice(
+    (currentPage - 1) * itemsPerPage,
+    currentPage * itemsPerPage
+  );
+
   return (
-    <div className="row">
-         <SearchBar  />
-      
+    <div className="row ">
+      <SearchBar />
+
       <div className="col-lg-10 mt-3 col-sm-12">
-        {isLoading ? ( // Use isLoading state to conditionally render the loading spinner
+        {isLoading ? (
           <div className="d-flex justify-content-center">
             <div className="loading-container">
               <img
@@ -23,18 +41,64 @@ const Pg_Product_List = () => {
               />
             </div>
           </div>
-        ) : pgs.length === 0 ? ( // Show a message if there's no data
-        <div className="d-flex justify-content-center" >
+        ) : currentItems.length === 0 ? (
+          <div className="d-flex justify-content-center">
             <div className="no-record-found fs-1">No record found.</div>
-        </div>
+          </div>
         ) : (
-          pgs.map((pgData, index) => (
+          currentItems.map((pgData, index) => (
             <Pg_Listing_Card key={index} pgData={pgData} />
           ))
         )}
       </div>
       <div className="col-lg-2  display-none-mobile">
-        <AdsCard/>
+        <AdsCard />
+      </div>
+
+      <div className="col-12 d-flex justify-content-center">
+        <nav aria-label="Page navigation example">
+          <ul className="pagination justify-content-center">
+            <li className={`page-item ${currentPage === 1 ? "disabled" : ""}`}>
+              <button
+                className="page-link"
+                onClick={() => paginate(currentPage - 1)}
+              >
+                Previous
+              </button>
+            </li>
+            {Array.from({ length: Math.ceil(pgs.length / itemsPerPage) }).map(
+              (_, index) => (
+                <li
+                  key={index}
+                  className={`page-item ${
+                    currentPage === index + 1 ? "active" : ""
+                  }`}
+                >
+                  <button
+                    className="page-link"
+                    onClick={() => paginate(index + 1)}
+                  >
+                    {index + 1}
+                  </button>
+                </li>
+              )
+            )}
+            <li
+              className={`page-item ${
+                currentPage === Math.ceil(pgs.length / itemsPerPage)
+                  ? "disabled"
+                  : ""
+              }`}
+            >
+              <button
+                className="page-link"
+                onClick={() => paginate(currentPage + 1)}
+              >
+                Next
+              </button>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   );
